17b44e72d56879030e29347dbe834d31a82db664
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-21 01:07:31 +0700

    kinda tired of this shit

--- routes/api/users.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const controller = require('../../controllers/users.controller');
-
-router.get('/getAll', controller.onGetAll);
-
-module.exports = router;
