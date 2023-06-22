e76742e0277a9710015a61e9380553ba62d2291b
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-22 18:33:28 +0700

    fix console env

--- services/users.service.js
+++ services/users.service.js
@@ -8,10 +8,10 @@ const methods = {
             try
             {
                 console.log({
-                    host: process.env.MYSQL_HOST,
-                    user: process.env.MYSQL_USER,
-                    password: process.env.MYSQL_SECRET,
-                    database: process.env.MYSQL_DB
+                    host: process.env.DB_HOST,
+                    user: process.env.DB_USERNAME,
+                    password: process.env.DB_PASSWORD,
+                    database: process.env.DB_DATABASE
                 });
                 let con = mysql.createConnection({
                     host: process.env.DB_HOST,
