c424055b2cdba91be6ec5d5ece092128cdc98f3c
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-24 20:16:33 +0700

    make example to json

--- controllers/example.controller.js
+++ controllers/example.controller.js
@@ -4,7 +4,10 @@ const method = {
     async onGetExample(req, res)
     {
         const data = fs.readFileSync('schema.json', 'utf8');
-        res.send(data);
+        res.send({
+            staus: true,
+            result: JSON.parse(data)
+        });
     }
 };
 
