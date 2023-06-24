f4a951a2a83d832f0af493c2d74708fd9b468a15
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-24 18:16:51 +0700

    add port to env variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(require('./routes'));
 
-const server = app.listen(3000, () =>
+const server = app.listen(process.env.SERVER_PORT, () =>
 {
     let port = server.address().port;
     console.log(`Server is running at PORT ${port}`);
