c7dc1720527a60068119e47ec2002a626339d940
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-24 18:03:27 +0700

    change port to 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(require('./routes'));
 
-const server = app.listen(80, () =>
+const server = app.listen(3000, () =>
 {
     let port = server.address().port;
     console.log(`Server is running at PORT ${port}`);
