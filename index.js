f70f038db28ae501971a5741c704b9cb18833b5f
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-06-25 03:35:14 +0700

    change origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://sbacprofile.blauenthepeople.com"
+        origin: "http://blauenthepeople.com"
     }));
 app.use(require('./routes'));
 
