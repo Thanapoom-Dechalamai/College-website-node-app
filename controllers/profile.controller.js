85231ac7f8734237ffd610966fd5973a240324d9
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-07-15 21:32:04 +0700

    fix key in profile object

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -21,7 +21,23 @@ const methods = {
                 console.log(result);
                 res.send({
                     status: true,
-                    result: result[0]
+                    result: {
+                        primary_profile_ID: result[0]['primary_student_ID'],
+                        profile_ID: result[0]['student_ID'],
+                        profile_first_name: result[0]['student_first_name'],
+                        profile_last_name: result[0]['student_last_name'],
+                        profile_nickname: result[0]['student_nickname'],
+                        profile_first_name_thai: result[0]['student_first_name_thai'],
+                        profile_last_name_thai: result[0]['student_last_name_thai'],
+                        profile_nickname_thai: result[0]['student_nickname_thai'],
+                        profile_major: result[0]['student_major'],
+                        profile_level: result[0]['student_level'],
+                        profile_class: result[0]['student_class'],
+                        profile_phone: result[0]['student_phone'],
+                        profile_line_ID: result[0]['student_line_ID'],
+                        profile_image: result[0]['student_image'],
+                        profile_email: result[0]['student_email']
+                    }
                 });
                 break;
             case 2:
@@ -29,7 +45,21 @@ const methods = {
                 console.log(result);
                 res.send({
                     status: true,
-                    result: result[0]
+                    result: {
+                        primary_profile_ID: result[0]['primary_teacher_ID'],
+                        profile_ID: result[0]['student_ID'],
+                        profile_first_name: result[0]['teacher_first_name'],
+                        profile_last_name: result[0]['teacher_last_name'],
+                        profile_nickname: result[0]['teacher_nickname'],
+                        profile_first_name_thai: result[0]['teacher_first_name_thai'],
+                        profile_last_name_thai: result[0]['teacher_last_name_thai'],
+                        profile_nickname_thai: result[0]['teacher_nickname_thai'],
+                        profile_major: result[0]['teacher_major'],
+                        profile_phone: result[0]['teacher_phone'],
+                        profile_line_ID: result[0]['teacher_line_ID'],
+                        profile_image: result[0]['teacher_image'],
+                        profile_email: result[0]['teacher_email']
+                    }
                 });
                 break;
         }
