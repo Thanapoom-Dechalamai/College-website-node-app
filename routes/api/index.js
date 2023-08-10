f6901635d3886ae7a63f5d82a322b96c2d39b791
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-08-10 18:12:08 +0700

    add document api

--- routes/api/index.js
+++ routes/api/index.js
@@ -10,5 +10,6 @@ router.use('/image', require('./image'));
 router.use('/profile', require('./profile'));
 router.use('/forms', require('./forms'));
 router.use('/upload', require('./upload-files'));
+router.use('/document', require('./document'));
 
 module.exports = router;
