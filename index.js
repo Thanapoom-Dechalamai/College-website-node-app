930396e634d23493d99c46cafe2b1ebc40a3a86a
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-08-26 17:09:00 +0700

    add allow origin sbacprofile.blauens.com

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://blauens.com", "http://blauenthepeople.com", "http://localhost:5173", "http://localhost:5174"]
+        origin: ["https://blauens.com", "https://sbacprofile.blauens.com", "http://blauenthepeople.com", "http://localhost:5173", "http://localhost:5174"]
     })
 );
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
