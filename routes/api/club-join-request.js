b2678572fd4297bf74a9694a9547b4c6b5671c7a
Blauen <85344725+Blauenn@users.noreply.github.com>
2023-11-07 18:49:40 +0700

    Revert "Add club join request API"
    This reverts commit 4fac9faff1d278ae0800296c8a7a38bbe99c94f6.

--- routes/api/club-join-request.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const controller = require('../../controllers/club-join-request.controller');
-
-router.get('/getAll', controller.onGetAll);
-router.post('/create', controller.onCreateOne);
-router.post('/update', controller.onUpdateAt);
-router.post('/remove', controller.onRemove);
-
-module.exports = router;
