9e9309f5482daddf69befc08efc1989b9f09124d
Blauen <85344725+Blauenn@users.noreply.github.com>
2023-12-01 23:22:19 +0700

    Update request-form.service.js

--- services/form/request-form.service.js
+++ services/form/request-form.service.js
@@ -4,115 +4,116 @@ const { requestForm_student_columns, requestForm_teacher_columns, requestForm_he
 require("dotenv").config();
 
 const methods = {
-    // Get all //
-    getAll() {
-        return new Promise((resolve, reject) => {
-            const query = `SELECT * FROM ${process.env.DB_TABLE_REQUEST_FORM}`;
-            db.query(query)
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        });
-    },
-    // Get one //
-    getOne(id) {
-        return new Promise((resolve, reject) => {
-            const query = `SELECT * FROM ${process.env.DB_TABLE_REQUEST_FORM} WHERE request_form_ID = ?`;
-            db.query(query, [id])
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        });
-    },
+	// Get all //
+	getAll() {
+		return new Promise((resolve, reject) => {
+			const query = `SELECT * FROM ${process.env.DB_TABLE_REQUEST_FORM}`;
+			db.query(query)
+				.then(result => {
+					resolve(result);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		});
+	},
+	// Get one //
+	getOne(id) {
+		return new Promise((resolve, reject) => {
+			const query = `SELECT * FROM ${process.env.DB_TABLE_REQUEST_FORM} WHERE request_form_ID = ?`;
+			db.query(query, [id])
+				.then(result => {
+					resolve(result);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		});
+	},
 
-    // Create //
-    createOne(object) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                object.request_form_create_datetime = dayjs().toISOString();
+	// Create //
+	createOne(object) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				object.request_form_create_datetime = dayjs().toISOString();
 
-                const columns = ["request_form_student_ID", "request_form_title, request_form_description, request_form_create_datetime, request_form_attached_file"];
-                const values = columns.map(column => object[column]);
-                const placeholders = new Array(values.length).fill("?").join(", ");
+				const columns = ["request_form_student_ID", "request_form_title, request_form_description, request_form_create_datetime, request_form_attached_file"];
+				const values = columns.map(column => object[column]);
+				const placeholders = new Array(values.length).fill("?").join(", ");
 
-                const sqlQuery = `INSERT INTO ${process.env.DB_TABLE_REQUEST_FORM} (${columns.join(", ")}) VALUES (${placeholders})`;
+				const sqlQuery = `INSERT INTO ${process.env.DB_TABLE_REQUEST_FORM} (${columns.join(", ")}) VALUES (${placeholders})`;
 
-                const results = await db.query(sqlQuery, values);
-                resolve(results);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    },
+				const results = await db.query(sqlQuery, values);
+				resolve(results);
+			} catch (error) {
+				console.log(error);
+				reject(error);
+			}
+		});
+	},
 
-    // Update //
-    updateOne(id, updateAs, object) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let allowedColumns;
-                switch (updateAs) {
-                    // If student updates //
-                    case 1:
-                        object.request_form_create_datetime = dayjs().toISOString();
-                        allowedColumns = requestForm_student_columns;
-                        break;
-                    // If teacher evaluates //
-                    case 2:
-                        object.request_form_teacher_change_datetime = dayjs().toISOString();
-                        allowedColumns = requestForm_teacher_columns;
-                        break;
-                    // If head evaluates //
-                    case 3:
-                        object.request_form_head_change_datetime = dayjs().toISOString();
-                        allowedColumns = requestForm_head_columns;
-                        break;
-                    default:
-                        allowedColumns = requestForm_student_columns.concat(requestForm_teacher_columns, requestForm_head_columns);
-                        break;
-                }
-                const columns = [];
-                const values = [];
-                for (const column of allowedColumns) {
-                    if (column in object) {
-                        columns.push(`${column} = ?`);
-                        values.push(object[column]);
-                    }
-                }
+	// Update //
+	updateOne(id, updateAs, object) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let allowedColumns;
+				switch (updateAs) {
+					// If student updates //
+					case 1:
+						object.request_form_create_datetime = dayjs().toISOString();
+						allowedColumns = requestForm_student_columns;
+						break;
+					// If teacher evaluates //
+					case 2:
+						object.request_form_teacher_change_datetime = dayjs().toISOString();
+						allowedColumns = requestForm_teacher_columns;
+						break;
+					// If head evaluates //
+					case 3:
+						object.request_form_head_change_datetime = dayjs().toISOString();
+						allowedColumns = requestForm_head_columns;
+						break;
+					default:
+						allowedColumns = requestForm_student_columns.concat(requestForm_teacher_columns, requestForm_head_columns);
+						break;
+				}
+				const columns = [];
+				const values = [];
+				for (const column of allowedColumns) {
+					if (column in object) {
+						columns.push(`${column} = ?`);
+						values.push(object[column]);
+					}
+				}
 
-                if (columns.length === 0) {
-                    return res.sendStatus(204);
-                }
+				if (columns.length === 0) {
+					return res.sendStatus(204);
+				}
 
-                const sqlQuery = `UPDATE ${process.env.DB_TABLE_REQUEST_FORM} SET ${columns.join(", ")} WHERE request_form_ID = ?`;
-                values.push(id);
+				const sqlQuery = `UPDATE ${process.env.DB_TABLE_REQUEST_FORM} SET ${columns.join(", ")} WHERE request_form_ID = ?`;
+				values.push(id);
 
-                const results = await db.query(sqlQuery, values);
-                resolve(results);
-            } catch (error) {
-                reject(error);
-            }
-        })
-    },
+				const results = await db.query(sqlQuery, values);
+				resolve(results);
+			} catch (error) {
+				reject(error);
+			}
+		});
+	},
 
-    // Delete //
-    deleteOne(id) {
-        return new Promise((resolve, reject) => {
-            const query = `DELETE FROM ${process.env.DB_TABLE_REQUEST_FORM} WHERE request_form_ID = ?`;
-            db.query(query, [id])
-                .then(result => {
-                    resolve(result);
-                })
-                .catch(error => {
-                    reject(error);
-                });
-        });
-    }
+	// Delete //
+	deleteOne(id) {
+		return new Promise((resolve, reject) => {
+			const query = `DELETE FROM ${process.env.DB_TABLE_REQUEST_FORM} WHERE request_form_ID = ?`;
+			db.query(query, [id])
+				.then(result => {
+					resolve(result);
+				})
+				.catch(error => {
+					reject(error);
+				});
+		});
+	}
 };
 
 module.exports = { ...methods };
