4c92266b6a952c41c09a4e2f025c3f8d7bd72c09
Blauen <85344725+Blauenn@users.noreply.github.com>
2023-12-01 23:27:48 +0700

    Quotation marks in the columns array
    I forgot to put them in.

--- constants/form/request-form.constant.js
+++ constants/form/request-form.constant.js
@@ -1,4 +1,4 @@
-const requestForm_student_columns = ["request_form_title, request_form_description, request_form_create_datetime, request_form_attached_file"];
+const requestForm_student_columns = ["request_form_title", "request_form_description", "request_form_create_datetime", "request_form_attached_file"];
 const requestForm_teacher_columns = ["request_form_teacher_ID", "request_form_teacher_status", "request_form_teacher_description", "request_form_teacher_change_datetime"];
 const requestForm_head_columns = ["request_form_head_ID", "request_form_head_status", "request_form_head_description", "request_form_head_change_datetime"];
 
